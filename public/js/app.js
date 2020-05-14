379c76ca83553985cb23cfeb6ddc19a283315cc0
Murugesh Aravind <murugesh@amazecodes.com>
2020-05-14 19:30:57 +0530

    changes made in the web-server

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side javascript file is loaded!');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let location = search.value;
+  console.log('location-->', location);
+  messageOne.textContent = 'Loading....';
+  messageTwo.textContent = '';
+  fetch(`http://localhost:3000/weather?address="${location}"`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error)
+          return (
+            (messageOne.textContent = data.error), (messageTwo.textContent = '')
+          );
+        console.log('Success response-->', data);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      });
+    },
+  );
+});
