c460a69e86deb330fcfa3f8c413dbc380e3ced48
Murugesh Aravind <murugesh@amazecodes.com>
2020-05-14 23:00:26 +0530

    changes made in the app js

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
-    name: 'Andrew Mead',
+    name: 'Murugesh Aravind',
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
-    name: 'Andrew Mead',
+    name: 'Murugesh Aravind',
   });
 });
 
@@ -37,7 +37,7 @@ app.get('/help', (req, res) => {
   res.render('help', {
     helpText: 'This is some helpful text.',
     title: 'Help',
-    name: 'Andrew Mead',
+    name: 'Murugesh Aravind',
   });
 });
 
@@ -79,7 +79,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Andrew Mead',
+    name: 'Murugesh Aravind',
     errorMessage: 'Help article not found.',
   });
 });
@@ -87,7 +87,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Andrew Mead',
+    name: 'Murugesh Aravind',
     errorMessage: 'Page not found.',
   });
 });
